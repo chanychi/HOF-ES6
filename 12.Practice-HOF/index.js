99ffc76891fd5d44b584208c43fb613c696518ea
chanychi <chan.ychi@gmail.com>
2022-01-12 18:11:39 -0600

    practice completed

---
+++ 12.Practice-HOF/index.js
@@ -0,0 +1,16 @@
+//Create
+let cart = [
+  {"name":"Biscuits", "type":"regular", "category":"food", "price": 2.0},
+  {"name":"Monitor", "type":"prime", "category":"tech", "price": 119.99},
+  {"name":"Mouse", "type":"prime", "category":"tech", "price": 25.50},
+  {"name":"dress", "type":"regular", "category":"clothes", "price": 49.90},
+]
+//Direction - build a function that return only the food with the type prime
+
+function doSomething (input) {
+  //do code
+};
+
+//
+
+console.log(doSomething(cart))
